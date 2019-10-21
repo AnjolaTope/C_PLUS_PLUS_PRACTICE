6538a1b1b3174eabd9f960f7bb76ed1d5c940fa1
AnjolaTope <56732553+AnjolaTope@users.noreply.github.com>
2019-10-20 18:40:19 -0700

    Create PRIME_NUMBER_CHECK.cpp

---
+++ PRIME_NUMBER_CHECK.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int num1, x = 0, z;
+
+	cout << " This is to Check whether a number is prime or not:";
+	cout << "Input a number to check prime or not:\n";
+	cin >>num1;
+	for (z = 1; z <= num1; z++)
+	{
+		if (num1 % z == 0)
+		{
+			x++;
+		}
+	}
+	if (x == 2)
+	{
+		cout << "The entered number is a prime number. \n";
+	}
+	else {
+		cout << "The number you entered is not a prime number. \n";
+	}
+
+
+	cout << "thank you";
+
+	return 0;
+
+}
+
+This is the link to run this code online: https://repl.it/@AnjolaTopeBabal/PRIMENUMBERCHECK
