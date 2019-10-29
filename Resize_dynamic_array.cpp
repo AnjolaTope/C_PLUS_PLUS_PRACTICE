b3fd4a0ca0c7273501a723126e9b24bf2f04c089
AnjolaTope <56732553+AnjolaTope@users.noreply.github.com>
2019-10-29 09:36:06 -0700

    Update Resize_dynamic_array.cpp

--- Resize_dynamic_array.cpp
+++ Resize_dynamic_array.cpp
@@ -3,50 +3,58 @@
 #include<string>
 using namespace std;
 
-
+// this is the morespace function that increases the size of the array
 int morespace(string* &p,int &size) {
   int s=0;
-	 s=size;
+	 s=size+10;
 	 string* pointer;
-	 pointer = new string[s+10];
+	 pointer = new string[s];
 
-	for (int i = 0; i < s; i++)
+	for (int i = 0; i < size; i++)
 	  {
 		 pointer[i] = p[i] ;
 	  }
   delete[]p;
 
-	size += 10;
+	
 	p = pointer;	
-  return size;
+  return s;
 }
 
 int main()
 {
+
 	ifstream file;
 	string* list;
 	int n = 10;
+        // this Initializes the original array. This array has ten elements 
 	list = new string[n];
-	file.open("list.txt");
 
+        cout <<"The initial  array contains: "<< n <<" elements"<<endl; 
+	file.open("List.txt",ios::in);
+        //this opens the file and inserts the first ten elements in the file into the array
 	if (file.is_open())
 	{
 		for (int i = 0; i < n; i++)
 		{
 			file >> list[i];
-			//cout << list[i] << endl;
+			cout << list[i] << endl;
 		}
 	}
-
-	while(!file.eof() )
-	{
-		if (list[n - 1] != "")
-		{
+       // this dynamically increases the size of the array when there are still elements in the file
+       while(!file.eof() )
+	 {
+		   if (list[n - 1] != "")
+		    {
+       // this is the morespace function that increases the size of the array
 			morespace(list, n);
-		}
-	
-		n= morespace(list, n);
-		
+		    }
+       // the morespace function returns the new size of the array 
+       // the new size of the array is put into the variable n 
+      cout <<"The new array contains: "<< n << " elements"<< endl; 
+		  n= morespace(list, n);
+   
+		// The elemnts in the file are added to the new array
 			for (int i = n-10; i < n; i++)
 			{
 				file >> list[i];
@@ -60,7 +68,6 @@ int main()
 
 	delete[]list;
 
-	system("pause");
 
 	return 0;
 
